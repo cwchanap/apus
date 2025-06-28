62e5ae9efb11b7aeb5da32124cbb14926dfb29e4
cwchanap <cwchanap@connect.ust.hk>
2025-06-27 22:11:09 -0700

    Initial Commit

---
+++ apusTests/apusTests.swift
@@ -0,0 +1,17 @@
+//
+//  apusTests.swift
+//  apusTests
+//
+//  Created by Chan Wai Chan on 27/6/2025.
+//
+
+import Testing
+@testable import apus
+
+struct apusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
